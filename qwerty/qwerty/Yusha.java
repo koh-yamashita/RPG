76f2d1230c54ba2c4138c75c61f35f22f4593cf2
koh-yamashita <46775111+koh-yamashita@users.noreply.github.com>
2019-04-01 23:18:51 +0900

    Delete Yusha.java

--- qwerty/qwerty/Yusha.java
+++
@@ -1,9 +0,0 @@
-package qwerty;
-class Yusha extends Chara {
-	Yusha(String name,int hp,int mp,int atk){
-		super(name,hp,mp,atk);
-	}
-	public void introduce() {
-		System.out.println("私は" + this.name + "です。HPは" + this.hp + "でMPは" + this.mp + "です。");
-	}
-}
